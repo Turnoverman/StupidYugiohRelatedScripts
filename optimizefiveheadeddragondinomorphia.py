9bd9efc73229a79f13e0902d2e14dca0e6b9a543
Turnoverman <turnoverman@yahoo.com>
2024-09-07 10:38:29 -0400

    I didn't mean to add the dump or those commented out bits

--- optimizefiveheadeddragondinomorphia.py
+++ optimizefiveheadeddragondinomorphia.py
@@ -32,26 +32,3 @@ for i in range(len(candidates)):
 					if (total < 8000) and ((8000-total) <= minimum_lp):
 						minimum_lp = 8000 - total
 						print(8000-total, candidates[i], candidates[j], candidates[k], candidates[l], candidates[m])
-#
-# deck = []
-# extradeck = []
-# num = 0
-# while num < 40:
-# 	id = int(random.random() * 100000000)
-# 	if id in ids:
-# 		num += 1
-# 		deck.append(id)
-# 	elif id in edids:
-# 		extradeck.append(id)
-#
-# # Output (to stdout, use pipes) a .ydk file
-# print("#main")
-# for card in deck:
-# 	print(card)
-# print("")
-# print("#extra")
-# for card in extradeck:
-# 	print(card)
-# print("")
-# print("!side")
-# print("")
